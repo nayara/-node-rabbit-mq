a0adf48ecaf8ac260bd343f1e158d6b1f9b38b01
Nayara Souza <nsouza@thoughtworks.com>
2020-03-30 19:29:13 -0300

    Creates tasks and workers

---
+++ tasks/new_task.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+const amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', (error0, connection) => {
+  if (error0) {
+    throw error0;
+  }
+
+  connection.createChannel((error1, channel) => {
+    if(error1) {
+      throw error1;
+    }
+
+    const queue = 'task_queue';
+    const message = process.argv.slice(2).join(' ') || 'Hello World!';
+
+    channel.assertQueue(queue, {
+      durable: true
+    });
+
+    channel.sendToQueue(queue, Buffer.from(message), { persistent: true });
+    console.log(" [X] Sent %s", message);
+  });
+
+  setTimeout(
+    () => {
+      connection.close();
+      process.exit(0) 
+    }, 500);
+});
