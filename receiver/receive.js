cd035cbf48b16bafef3c3acccd0adab67725be41
Nayara Souza <nsouza@thoughtworks.com>
2020-04-01 16:48:00 -0300

    Fix merge errors

--- receiver/receive.js
+++ receiver/receive.js
@@ -1,11 +1,6 @@
 #!/usr/bin/env node
 
 const amqp = require('amqplib/callback_api');
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> c588ba8... temp
 const convertToObject = (message) => JSON.parse(message);
 
 const handleEvent = (message) => {
@@ -13,11 +8,6 @@ const handleEvent = (message) => {
   console.log("[x] Received %s %s", obj.name, obj.surname);
 };
 
-<<<<<<< HEAD
-=======
->>>>>>> Creates a simple sender and receiver
-=======
->>>>>>> c588ba8... temp
 amqp.connect('amqp://localhost', (error0, connection) => {
   if (error0) {
     throw error0;
@@ -38,15 +28,7 @@ amqp.connect('amqp://localhost', (error0, connection) => {
 
     channel.consume(
       queue,
-<<<<<<< HEAD
-<<<<<<< HEAD
       message => handleEvent(message.content),
-=======
-      message => console.log("[x] Received %s", message.content.toString()),
->>>>>>> Creates a simple sender and receiver
-=======
-      message => handleEvent(message.content),
->>>>>>> c588ba8... temp
       { noAck: true }
     ); 
   });
